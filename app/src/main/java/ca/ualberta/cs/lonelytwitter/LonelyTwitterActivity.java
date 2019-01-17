ca68b78e751505a3cf5bb6d8c2a48ab98fe76153
Dimitri Trofimuk <dimitri.trofimuk@ualberta.ca>
2019-01-17 15:06:46 -0700

    Save and clear buttons

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,16 +1,14 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Date;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,12 +16,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class LonelyTwitterActivity extends Activity {
 
-	private static final String FILENAME = "file.sav";
+	private static final String FILENAME = "file7.sav";
+
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	
+	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
+	private ArrayAdapter<Tweet> adapter;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,40 +36,56 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
-
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
 
+				Tweet tweet = new Tweet(text);
+
+				tweetList.add(tweet);
+
+				adapter.notifyDataSetChanged();
+				saveInFile();
 			}
 		});
+
+		clearButton.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+		        setResult(RESULT_OK);
+
+		        tweetList.clear();
+		        adapter.notifyDataSetChanged();
+		        saveInFile();
+            }
+        });
 	}
 
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+
+		loadFromFile();
+
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
+
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	private void loadFromFile() {
 		try {
-			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+			FileReader in = new FileReader(new File(getFilesDir(),FILENAME));
+			Gson gson = new Gson();
+
+			Type listtype = new TypeToken<ArrayList<Tweet>>(){}.getType();
+
+			tweetList = gson.fromJson(in, listtype);
+
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -74,16 +94,16 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+
 	}
-	
-	private void saveInFile(String text, Date date) {
+
+	private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
-			fos.close();
+			FileWriter out = new FileWriter(new File(getFilesDir(),FILENAME));
+
+			Gson gson = new Gson();
+			gson.toJson(tweetList,out);
+			out.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,29 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class Tweet {
+    String message;
+    Date date;
+
+    Tweet() {
+
+    }
+
+    Tweet(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public String toString(){
+        return this.message;
+    }
+}
